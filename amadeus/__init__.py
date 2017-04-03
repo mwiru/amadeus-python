d59275d90d05834d5bf8b6ae0c7e0d8cde7335f5
mwiru <mwinel@live.com>
2017-04-03 11:08:37 +0300

    Update __init__.py

--- amadeus/__init__.py
+++ amadeus/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Ardy Dedase'
-__email__ = 'ardy.dedase@gmail.com'
+__author__ = 'Nelson Mwiru'
+__email__ = 'nelsonmwirumubi@gmail.com'
 __version__ = '0.1.1'
 
 from .amadeus import Flights, Hotels
